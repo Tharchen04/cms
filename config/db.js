d9df94608b1bbf529cbac8c9a5c30ad52561a19b
Tharchen04 <thaarcheen@gmail.com>
2025-06-04 20:11:04 +0600

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,9 @@ const pgp = require('pg-promise')();
 require('dotenv').config();
 
 const db = pgp({
-  host: 'localhost',
-  port: 5432,
-  database: 'cms_db',       // your DB name created in pgAdmin
-  user: 'postgres',      // usually 'postgres'
-  password: '123',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // required by Render PostgreSQL
+    rejectUnauthorized: false,
   }
 });
 
